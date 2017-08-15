a790c2ac69bb81fb4cadf788680da1a24aa3550c
autohome <day_clear.com>
2017-08-15 16:08:25 +0800

    增加前一页和后一页的文本配置选项

--- src/jquery-paging.js
+++ src/jquery-paging.js
@@ -1,67 +1,70 @@
 (function(){
 
-  function Paging($el, option) {
+  function Paging($el, options) {
     
-    var option = option || {
-      total: 10,
-      cur: 1,
-      max: 8,
-      cb: function(index){return false;}
-    }
-
-    this.total = option.total;
-    this.cur = option.cur;
-    this.max = option.max > 5 ? option.max : 5;
-    this.cb = option.cb;
+    var options = options || {};
+    this.total = options.total || 10;
+    this.cur = options.cur || 1;
+    this.max = options.max ? options.max > 5 ? options.max : 5 : 8;
+    this.prevText = options.prev_text || '<',
+    this.nextText = options.next_text || '>',
+    this.cb = options.cb || function(index){return false;};
     this.$el = $el;
 
     this.init();
   }
 
   Paging.prototype = {
+
+    // 初始化
     init: function() {
       this.show();
     },
+    //显示页标签
     show: function() {
       var $ul = $('<ul>').attr('class', 'paging-ul');
+      var _this = this;
+      var t = _this.total;
+      var c = _this.cur;
+      var m = _this.max;
 
-      this.addItem($ul, -1);
-      if (this.total > this.max) {
+      _this.addItem($ul, -1);
+
+      if (t > m) {
         
-        if (this.total - this.cur < this.max - 2) {
-          this.addItem($ul, 1);
-          this.addItem($ul, -111);
-          for (var i = this.total - this.max + 3 ; i <= this.total; i++) {
-            this.addItem($ul, i);
+        if (t - c < m - 2) {
+          _this.addItem($ul, 1).addItem($ul, -111);
+          for (var i = t - m + 3 ; i <= t; i++) {
+            _this.addItem($ul, i);
           };
-        } else if (this.cur - 1 < this.max - 2) {
-          for (var i = 1 ; i <= this.max - 2; i++) {
-            this.addItem($ul, i);
+        } else if (c - 1 < m - 2) {
+          for (var i = 1 ; i <= m - 2; i++) {
+            _this.addItem($ul, i);
           };
-          this.addItem($ul, -111);
-          this.addItem($ul, this.total);
+          _this.addItem($ul, -111).addItem($ul, t);
         } else {
-          this.addItem($ul, 1);
-          this.addItem($ul, -111);
-          for (var i = -Math.floor((this.max - 5)/2)  ; i <= Math.ceil((this.max - 5)/2); i++) {
-            this.addItem($ul, this.cur + i);
+          _this.addItem($ul, 1).addItem($ul, -111);
+          for (var i = -Math.floor((m - 5)/2)  ; i <= Math.ceil((m - 5)/2); i++) {
+            _this.addItem($ul, c + i);
           };
-          this.addItem($ul, -111);
-          this.addItem($ul, this.total);
+          _this.addItem($ul, -111).addItem($ul, t);
         }
 
       } else {
 
         for (var i = 1 ; i <= this.total; i++) {
-          this.addItem($ul, i);
+          _this.addItem($ul, i);
         };
       }
-      this.addItem($ul, -11);
 
-      this.$el.empty();
-      this.$el.append($ul);
-      this.cb(this.cur);
+      _this.addItem($ul, -11);
+
+      _this.$el.empty();
+      _this.$el.append($ul);
+      _this.cb(this.cur);
     },
+
+    // 数组里面添加页标签
     addItem: function($ul, index) {
       var $item = null;
       var _this = this;
@@ -69,10 +72,10 @@
       if (index == -1) {
 
         if (_this.cur === 1) {
-          $item = $('<li>&lt</li>')
+          $item = $('<li>' + _this.prevText + '</li>')
                     .attr('class', 'paging-prev paging-prev-disable');;
         } else {
-          $item = $('<a><li>&lt</li></a>')
+          $item = $('<a><li>' + _this.prevText + '</li></a>')
                   .bind('click', function(){_this.prev()})
                     .attr('href', 'javascript:void(0)')
                       .attr('class', 'paging-prev');
@@ -80,11 +83,11 @@
 
       } else if (index === -11) {
 
-        if (_this.cur === this.total) {
-          $item = $('<li>&gt</li>')
+        if (_this.cur === _this.total) {
+          $item = $('<li>' + _this.nextText + '</li>')
                     .attr('class', 'paging-next paging-next-disable');
         } else {
-          $item = $('<a><li>&gt</li></a>')
+          $item = $('<a><li>' + _this.nextText + '</li></a>')
                   .bind('click', function(){_this.next()})
                     .attr('href', 'javascript:void(0)')
                       .attr('class', 'paging-next');
@@ -109,15 +112,22 @@
       };
 
       $ul.append($item);
+      return _this;
     },
+
+    // 跳转至具体页面
     goto: function(index) {
       this.cur = index;
       this.init();
     },
+
+    // 上一页
     prev: function() {
       this.cur--;
       this.init();
     },
+
+    // 下一页
     next: function() {
       this.cur++;
       this.init();
